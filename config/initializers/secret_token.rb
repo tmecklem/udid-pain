3ac51b6e11248ceb72c50ac68ea7e452c9d6c1d9
Timothy Mecklem <tmecklem@medplus.com>
2013-07-17 15:36:05 -0400

    initial commit for the udid pain app

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+UdidPain::Application.config.secret_token = '6515d4d2bdb0ba4f1d0e807e96ae76524e3835efabeea867476dd501cb339495f8e488352a027acadf2c6aa1b13d740ecbd9d345292dba7192ba7fe66caf37ec'
